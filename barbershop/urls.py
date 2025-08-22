00782ebd77a51de8a29205f9c6768cbc47f4d07f
kategolovina84 <kategolovina84@yandex.ru>
2025-08-23 00:57:19 +0500

    hw_38: создание vмаршрутов landing, thanks, orders list, order_detail,

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -1,22 +1,13 @@
-"""
-URL configuration for barbershop project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# barbershop/urls.py
 from django.contrib import admin
 from django.urls import path
+from core.views import landing, thanks, orders_list, order_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', landing),
+    path('thanks/', thanks),
+    path('orders/', orders_list),
+    path('orders/<int:order_id>/', order_detail),
+    ]
+
--- core/views.py
+++ core/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+# core/views.py
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+def landing(request) -> HttpResponse:
+    '''
+    Отвечает за маршрут '/'
+    '''
+    return HttpResponse("<h1>Главная страница</h1>")
+
+def thanks(request) -> HttpResponse:
+    '''
+    Отвечает за маршрут 'tanks/'
+    '''
+    return HttpResponse("<h1>Спасибо за заявку</h1>")
+
+def orders_list(request) -> HttpResponse:
+    '''
+    Отвечает за маршрут 'orders/'
+    '''
+    return HttpResponse("<h1>Список заявок</h1>")
+
+def order_detail(request, order_id) -> HttpResponse:
+    '''
+    Отвечает за маршрут 'orders/<int:order_id>/'
+    param request: HttpRequest
+    param order_id: id заявки
+    '''
+    return HttpResponse(f"<h1>Детали заявки {order_id}</h1>")
+
