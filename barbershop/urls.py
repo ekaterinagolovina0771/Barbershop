2f904ac6a9a9a19d3e31fc242f08628aa6dcaf37
kategolovina84 <kategolovina84@yandex.ru>
2025-08-24 00:05:53 +0500

    hw_39: static

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -1,4 +1,6 @@
 # barbershop/urls.py
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from core.views import landing, thanks, orders_list, order_detail
@@ -9,5 +11,5 @@ urlpatterns = [
     path('thanks/', thanks),
     path('orders/', orders_list),
     path('orders/<int:order_id>/', order_detail),
-    ]
+    ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
 
