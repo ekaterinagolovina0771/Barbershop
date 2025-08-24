579904ff051b5f10c2799bcf0d2676f7f1ec717f
kategolovina84 <kategolovina84@yandex.ru>
2025-08-24 11:50:18 +0500

    hw_40: Создание модели Заказ

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ("not_approved", "Новая"),
+        ("approved", "Подтверждена"),
+        ("canceled", "Отменена"),
+        ("completed", "Завершена"),
+    ]
+    client_name = models.CharField(max_length=100, verbose_name="Имя клиента")
+    phone = models.CharField(max_length=20, verbose_name="Телефон")
+    comment = models.TextField(blank=True, verbose_name="Комментарий")
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="not_approved", verbose_name='Статус')
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    date_updated = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+    master = models.ForeignKey("Master", on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Мастер')
+    services = models.ManyToManyField("Service", related_name='orders', verbose_name='Услуги')
+    appointment_date = models.DateTimeField(verbose_name="Дата и время приема")
+
