1bced71d463185a602ac67848d2084e063c1a008
kategolovina84 <kategolovina84@yandex.ru>
2025-09-10 21:19:28 +0500

    убрала

--- core/views.py
+++ core/views.py
@@ -171,72 +171,72 @@ class OrderListView(ListView):
         return orders
 
 
-def orders_list(request):
-    """
-    Отвечает за маршрут 'orders/'
-    """
-    # Получаю из GET запроса все данные URL
-    # ПОИСКОВАЯ ФОРМА
-    search_query = request.GET.get("q", "")
-    # ЧЕКБОКСЫ выборки по полям
-    # 1. поиск по телефону - search_by_phone
-    # 2. поиск по имени - search_by_name
-    # 3. поиск по тексту комментария - search_by_comment
-    checkbox_name = request.GET.get("search_by_name", "")
-    checkbox_phone = request.GET.get("search_by_phone", "")
-    checkbox_comment = request.GET.get("search_by_comment", "")
-    # ЧЕКББОКСЫ выборки по статусам
-    # status_new
-    # status_confirmed
-    # status_completed
-    # status_canceled
-    checkbox_status_new = request.GET.get("status_new", "")
-    checkbox_status_confirmed = request.GET.get("status_confirmed", "")
-    checkbox_status_completed = request.GET.get("status_completed", "")
-    checkbox_status_canceled = request.GET.get("status_canceled", "")
+# def orders_list(request):
+#     """
+#     Отвечает за маршрут 'orders/'
+#     """
+#     # Получаю из GET запроса все данные URL
+#     # ПОИСКОВАЯ ФОРМА
+#     search_query = request.GET.get("q", "")
+#     # ЧЕКБОКСЫ выборки по полям
+#     # 1. поиск по телефону - search_by_phone
+#     # 2. поиск по имени - search_by_name
+#     # 3. поиск по тексту комментария - search_by_comment
+#     checkbox_name = request.GET.get("search_by_name", "")
+#     checkbox_phone = request.GET.get("search_by_phone", "")
+#     checkbox_comment = request.GET.get("search_by_comment", "")
+#     # ЧЕКББОКСЫ выборки по статусам
+#     # status_new
+#     # status_confirmed
+#     # status_completed
+#     # status_canceled
+#     checkbox_status_new = request.GET.get("status_new", "")
+#     checkbox_status_confirmed = request.GET.get("status_confirmed", "")
+#     checkbox_status_completed = request.GET.get("status_completed", "")
+#     checkbox_status_canceled = request.GET.get("status_canceled", "")
 
-    # РАДИОКНОПКА Порядок сортировки по дате
-    # order_by_date - desc, asc
-    order_by_date = request.GET.get("order_by_date", "desc")
+#     # РАДИОКНОПКА Порядок сортировки по дате
+#     # order_by_date - desc, asc
+#     order_by_date = request.GET.get("order_by_date", "desc")
 
-    # 1. Создаем Q-объект для текстового поиска
-    search_q = Q()
-    if search_query:
-        # Внутренние условия поиска объединяем через ИЛИ (|=)
-        if checkbox_phone:
-            search_q |= Q(phone__icontains=search_query)
-        if checkbox_name:
-            search_q |= Q(name__icontains=search_query)
-        if checkbox_comment:
-            search_q |= Q(comment__icontains=search_query)
+#     # 1. Создаем Q-объект для текстового поиска
+#     search_q = Q()
+#     if search_query:
+#         # Внутренние условия поиска объединяем через ИЛИ (|=)
+#         if checkbox_phone:
+#             search_q |= Q(phone__icontains=search_query)
+#         if checkbox_name:
+#             search_q |= Q(name__icontains=search_query)
+#         if checkbox_comment:
+#             search_q |= Q(comment__icontains=search_query)
 
-    # 2. Создаем Q-объект для фильтрации по статусам
-    status_q = Q()
-    # Условия статусов тоже объединяем через ИЛИ (|=)
-    if checkbox_status_new:
-        status_q |= Q(status="new")
-    if checkbox_status_confirmed:
-        status_q |= Q(status="confirmed")
-    if checkbox_status_completed:
-        status_q |= Q(status="completed")
-    if checkbox_status_canceled:
-        status_q |= Q(status="canceled")
+#     # 2. Создаем Q-объект для фильтрации по статусам
+#     status_q = Q()
+#     # Условия статусов тоже объединяем через ИЛИ (|=)
+#     if checkbox_status_new:
+#         status_q |= Q(status="new")
+#     if checkbox_status_confirmed:
+#         status_q |= Q(status="confirmed")
+#     if checkbox_status_completed:
+#         status_q |= Q(status="completed")
+#     if checkbox_status_canceled:
+#         status_q |= Q(status="canceled")
 
-    # Порядок сортировки
-    ordering = "-date_created" if order_by_date == "desc" else "date_created"
+#     # Порядок сортировки
+#     ordering = "-date_created" if order_by_date == "desc" else "date_created"
 
-    # 3. Объединяем два Q-объекта через И (&)
-    # Это гарантирует, что запись должна соответствовать И условиям поиска, И условиям статуса
-    orders = (
-        Order.objects.prefetch_related("services")
-        .select_related("master")
-        .filter(search_q & status_q)
-        .order_by(ordering)
-    )
+#     # 3. Объединяем два Q-объекта через И (&)
+#     # Это гарантирует, что запись должна соответствовать И условиям поиска, И условиям статуса
+#     orders = (
+#         Order.objects.prefetch_related("services")
+#         .select_related("master")
+#         .filter(search_q & status_q)
+#         .order_by(ordering)
+#     )
 
-    context = {"orders": orders}
+#     context = {"orders": orders}
 
-    return render(request, "orders_list.html", context=context)
+#     return render(request, "orders_list.html", context=context)
 
 
 class OrderDetailView(DetailView):
